cb25c454624a0e5eed15f37fb1bc8d5adafa02fd
carlyls <carlyls320@gmail.com>
2023-03-16 16:34:34 -0400

    add bootstrap code

--- R/MA_OOSEst.R
+++ R/MA_OOSEst.R
@@ -1,15 +1,11 @@
 ## Trying Methods for OOS Estimation in Random Effects Meta-Analysis ##
 
 library(tidyverse)
-library(rsample)
-library(grf)
-library(fastDummies)
-library(nnet)
 library(lme4)
-library(pimeta)
 library(multcomp)
-library(merTools)
-library(marginaleffects)
+library(MASS)
+#library(pimeta)
+#library(merTools)
 
 source("R/MDD_Generation_OOSEst.R")
 
@@ -45,6 +41,7 @@ sum <- summary(mod)
 
 
 #prediction interval by hand ####
+#get variance components of model
 matrix_var <- function(mod) {
 
   #calculate variances
@@ -61,8 +58,9 @@ matrix_var <- function(mod) {
   var_rand <- matrix(rand, nrow=2, dimnames=list(c("W","W:age"),c("W","W:age"))) #Var(ranef) 
   
   return(list(beta=beta, var_beta=var_beta, var_rand=var_rand))
-} #get variance components of model
+}
 
+#add pis to dataset
 manual_pi <- function(df, mod, K) {
   
   #get variances
@@ -90,10 +88,11 @@ manual_pi <- function(df, mod, K) {
            upper = pred_upper)
   
   return(df)
-} #add pis to dataset
+}
 
 
 #confidence interval by glht ####
+#get ci based on x=age (as a character)
 age_ci <- function(x, mod) {
   
   ci <- glht(mod, linfct=paste0("W +",x,"*W:age = 0"))
@@ -103,8 +102,9 @@ age_ci <- function(x, mod) {
   upper <- mean + 1.96*sd %>% as.numeric()
   
   return(c(lower=lower, mean=mean, upper=upper))
-} #get ci based on x=age (as a character)
+} 
 
+#add cis to dataset
 glht_ci <- function(df, mod) {
   
   ages <- as.character(df$age)
@@ -113,26 +113,50 @@ glht_ci <- function(df, mod) {
     bind_cols(cis)
   
   return(df)
-} #add cis to dataset
+} 
 
 
 #prediction interval by bootstrap ####
+#create intervals from bootstrap
+sample_cate <- function(x, boot_fix, boot_rand) {
+  
+  #get cate|x according to each coefficient
+  cates <- (boot_fix$W + boot_rand$W) + 
+    (boot_fix$W.age + boot_rand$W.age)*x
+  
+  #calculate interval over all iterations
+  mean <- mean(cates)
+  sd <- sd(cates)
+  lower <- mean - 1.96*sd
+  upper <- mean + 1.96*sd
 
+  return(c(lower=lower, mean=mean, upper=upper))
+}
+
+#bootstrap and add intervals to data
+boot_pi <- function(df, mod) {
+  
+  #get var-covar of fixed and random effects from model
+  res <- matrix_var(mod) 
+  
+  #randomly sample fixed and random coefficients
+  boot_fix <- mvrnorm(n=1000, mu=c(res$beta), Sigma=res$var_beta) %>%
+    data.frame()
+  boot_rand <- mvrnorm(n=1000, mu=c(0,0), Sigma=res$var_rand) %>%
+    data.frame() #assume ranefs have mean 0
+  
+  #apply coefficients to estimate cate for all ages
+  intervals <- map_dfr(.x=df$age, .f=sample_cate,
+                             boot_fix=boot_fix, boot_rand=boot_rand)
+  df <- df %>%
+    bind_cols(intervals)
+  
+  return(df)
+}
 
 
 #check results for all methods ####
-assess_interval <- function(mod, train_dat, target_dat, method) {
-  
-  #add intervals to both datasets
-  if (method == "glht") {
-    train_dat <- glht_ci(train_dat, mod)
-    target_dat <- glht_ci(target_dat, mod)
-    
-  } else if (method == "manual") {
-    train_dat <- manual_pi(train_dat, mod)
-    target_dat <- manual_pi(target_dat, mod)
-    
-  }
+assess_interval <- function(train_dat, target_dat) {
   
   #calculate mse
   train_mse <- mean((train_dat$mean - train_dat$tau)^2)
@@ -142,13 +166,15 @@ assess_interval <- function(mod, train_dat, target_dat, method) {
   train_coverage <- sum(train_dat$tau >= train_dat$lower & train_dat$tau <= train_dat$upper)/nrow(train_dat)
   target_coverage <- sum(target_dat$tau >= target_dat$lower & target_dat$tau <= target_dat$upper)/nrow(target_dat)
   
+  #calculate length
+  train_length <- mean(train_dat$upper - train_dat$lower)
+  target_length <- mean(target_dat$upper - target_dat$lower)
+  
   return(c(train_mse = train_mse, target_mse = target_mse,
-           train_coverage = train_coverage, target_coverage = target_coverage))
+           train_coverage = train_coverage, target_coverage = target_coverage,
+           train_length = train_length, target_length = target_length))
   
 }
-
-
-
 
 
 #overall function ####
@@ -163,30 +189,32 @@ compare_oos <- function(N=100, K=6, n_mean=200, n_sd=0, eps_study_m=0.05,
   
   covars <- c("sex", "smstat", "weight", "age", "madrs")
   
+  
   ## Fit mixed effects model
   mod <- lmer(Y ~ W + age + W:age +
                        (W + age + W:age | S), data=train_dat)
   sum <- summary(mod)
   
-  #mse for training data
   
-  train_mse <- mean(( - tau_true)^2)
-  #CI coverage for training data
-  train_coverage <- sum(ifelse(tau_true >= lower & tau_true <= upper, 1, 0))/nrow(train_dat)
-  train_res <- c(train_mse=train_mse, train_coverage=train_coverage)
-  
-  
-  ## Calculate mean and CIs for each target individual according to each imputation method
-  #bootstrap PI
-  
-  #manual calculation
-  
+  ## Calculate mean and CIs for individual and assess accuracy
   #confidence interval
-  
+  glht_train <- glht_ci(train_dat, mod)
+  glht_target <- glht_ci(target_dat, mod)
+  glht_res <- assess_interval(glht_train, glht_target)
+    
+  #manual PI
+  manual_train <- manual_pi(train_dat, mod)
+  manual_target <- manual_pi(target_dat, mod)
+  manual_res <- assess_interval(manual_train, manual_target)
+
+  #bootstrap PI
+  boot_train <- boot_pi(train_dat, mod)
+  boot_target <- boot_pi(target_dat, mod)
+  boot_res <- assess_interval(boot_train, boot_target)
   
   
   ## Save results
-  return(list(
+  return(list(sum=sum, glht_res=glht_res, manual_res=manual_res, boot_res=boot_res,
               N=N, K=K, n_mean=n_mean, n_sd=n_sd,
               eps_study_m=eps_study_m, eps_study_tau=eps_study_tau, 
               distribution=distribution, target_dist=target_dist))
